a6f4f664c047711b251dad8bf2a2fb9e6d94bec0
CookiesKush420 <99442285+Callumgm@users.noreply.github.com>
2022-06-06 21:43:36 +0100

    Added obfuse code option for extra security

--- Clean_Builder.py
+++ Clean_Builder.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from time import sleep
 from colorama import Fore
 from util.build import *
@@ -18,20 +18,24 @@ def main():
         {Fore.GREEN}https://github.com/Callumgm
 
     {Fore.RESET}[{Fore.GREEN}1{Fore.RESET}] {Fore.CYAN}Clean Compile File
-    {Fore.RESET}[{Fore.GREEN}2{Fore.RESET}] {Fore.LIGHTRED_EX}Exit Builder
+    {Fore.RESET}[{Fore.GREEN}420{Fore.RESET}] {Fore.LIGHTRED_EX}Exit Builder
     
     """)
 
-    choice = str(input(f"{Fore.LIGHTMAGENTA_EX}Choose a number {Fore.RESET}>> "))
+    choice = str(input(f"{Fore.LIGHTMAGENTA_EX}Choose a number {Fore.YELLOW}>> "))
 
     if choice == "1":
-        fileName = str(input(f"{Fore.CYAN}Enter the name of the file u wish to compile {Fore.RESET}>> "))
-        scriptName = str(input(f"{Fore.CYAN}Enter output name for comiled exe file {Fore.RESET}>> "))
+        fileName = str(input(f"{Fore.CYAN}Enter the name of the file u wish to compile {Fore.YELLOW}>> "))
+        scriptName = str(input(f"{Fore.CYAN}Enter output name for comiled exe file {Fore.YELLOW}>> "))
+        obfuse = bool(input(f"{Fore.CYAN}Do u want to obfuscate source code for extra security? {Fore.RESET}({Fore.CYAN}Leave empty for no{Fore.RESET}) {Fore.YELLOW}>> "))
         clear()
-        builder(fileName, scriptName)
+        builder(fileName, scriptName, obfuse)
 
-    elif choice == "2":
-        os._exit(0)
+    elif choice == "420":
+        clear()
+        print(f"{Fore.CYAN}You will be missed :) {Fore.RESET}")
+        sleep(2)
+        sys.exit(0)
     
     else:
         print(f"{Fore.LIGHTRED_EX}Please choose a number!{Fore.RESET}")
--- util/build.py
+++ util/build.py
@@ -1,13 +1,43 @@
 import os
 import shutil
+import base64
+import random
+from Crypto.Cipher import AES
+from Crypto import Random
 from colorama import Fore
 from util.plugins.common import *
 
-def builder(fileName, scriptName):
+temp = f'C:\\Users\\{os.getlogin()}\\AppData\\Local\\Temp'
+
+def builder(fileName, scriptName, obfuse):
     setTitle(f"Compiling {fileName}.py => {scriptName}.exe") 
     print(f"\n{Fore.RESET}Compiling {Fore.GREEN}{fileName}.py {Fore.RESET}into {Fore.RED}{scriptName}.exe{Fore.YELLOW}\n")
     try:
-        os.system(f"pyinstaller --clean --onefile -n {scriptName} {fileName}.py") 
+        if obfuse == True:
+            IV = Random.new().read(AES.block_size)
+            key = u''
+            for i in range(8):
+                key = key + chr(random.randint(0x4E00, 0x9FA5))
+
+            with open(temp + f'\\{fileName}.py') as f: 
+                _file = f.read()
+                imports = ''
+                input_file = _file.splitlines()
+                for i in input_file:
+                    if i.startswith("import") or i.startswith("from"):
+                        imports += i+';'
+
+            with open(temp + f'\\{fileName}.py', "wb") as f:
+                encodedBytes = base64.b64encode(_file.encode())
+                obfuscatedBytes = AES.new(key.encode(), AES.MODE_CFB, IV).encrypt(encodedBytes)
+                f.write(f'from Crypto.Cipher import AES;{imports}exec(__import__(\'\\x62\\x61\\x73\\x65\\x36\\x34\').b64decode(AES.new({key.encode()}, AES.MODE_CFB, {IV}).decrypt({obfuscatedBytes})).decode())'.encode())
+            
+            shutil.move(temp + f'\\{fileName}.py', f"{os.getcwd()}\\{fileName}.py")
+
+
+        os.system(f"pyinstaller --onefile --clean --log-level=INFO -n {fileName} {fileName}.py")
+
+
         shutil.move(f"{os.getcwd()}\\dist\\{scriptName}.exe", f"{os.getcwd()}\\{scriptName}.exe") 
         shutil.rmtree('build')
         shutil.rmtree('dist')
@@ -25,3 +55,5 @@ def builder(fileName, scriptName):
         setTitle("Finished Creating exe!")
         print(f"\n{Fore.GREEN}Finished Creating exe\n")
     input(f'{Fore.RESET}[{Fore.GREEN}>>>]{Fore.RESET} Enter anything to continue . . .')
+
+        
